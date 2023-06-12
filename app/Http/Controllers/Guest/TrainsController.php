4eb11a2aead549f5f887e37c901619876bd7fba1
TullioTambone <tulliotambone97@gmail.com>
2023-06-12 14:41:00 +0200

    creazione model

--- app/Http/Controllers/Guest/TrainsController.php
+++ app/Http/Controllers/Guest/TrainsController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class TrainsController extends Controller
 {
     public function getTrain(){
-        return view('welcome');
+        $trains = Train::All();
+        return view('welcome', compact('trains'));
     }
 }
