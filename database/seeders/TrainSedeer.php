c5f559d3f5bcd341edc504a1fb115764b1450120
TullioTambone <tulliotambone97@gmail.com>
2023-06-12 15:13:34 +0200

    creazione elementi tramite seeder con il faker

--- database/seeders/TrainSedeer.php
+++ database/seeders/TrainSedeer.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Train;
+use Faker\Generator as Faker;
 class TrainSedeer extends Seeder
 {
     /**
@@ -12,8 +13,20 @@ class TrainSedeer extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run( Faker $faker )
     {
-        //
+        for ($i=0; $i < 10; $i++) { 
+            $newTrain = new Train();
+            $newTrain->Azienda = $faker->randomElement(['Italo', 'Trenitalia']);
+            $newTrain->Stazione_di_partenza = $faker->city();
+            $newTrain->Stazione_di_arrivo = $faker->city();
+            $newTrain->Orario_di_partenza = $faker->dateTime();
+            $newTrain->Orario_di_arrivo = $faker->dateTime();
+            $newTrain->Codice_treno = $faker->bothify('######');
+            $newTrain->Numero_carrozze = $faker->numberBetween(1, 9);
+            $newTrain->In_orario = $faker->randomElement([true, false]);
+            $newTrain->Cancellato = $faker->randomElement([true, false]);
+            $newTrain->save();
+        }
     }
 }
